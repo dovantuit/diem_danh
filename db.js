f6fb9832eec81e29a0c4e2023336924ee38e2249
vmuser <vmuser@somebody-vot.sonusnet.com>
2020-01-02 05:02:58 -0600

    danh chinh lai theo TMA

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const sequelize = require('sequelize')
+const sequelize = require('sequelize') // ORM - Hirbernate
 
 const db = new sequelize({
     database: 'DiemDanh',
@@ -15,60 +15,27 @@ const db = new sequelize({
     }
 })
 
-// db.authenticate()
-//     .then(() => console.log('>>>>>> ket noi thanh cong'))
-//     .catch(err => console.log(err.message))
+db.authenticate()
+    .then(() => console.log('>>> ket noi thanh cong'))
+    .catch(err => console.log(err.message))
 
 const accounts = db.define('accounts', { // model : table in database
     email: sequelize.STRING,
     password: sequelize.STRING,
     full_name: sequelize.STRING,
-    roll: sequelize.STRING,
-    phone_number: sequelize.STRING,
-    address: sequelize.STRING,
-    is_active: sequelize.BOOLEAN
+    birthday: sequelize.DATE
 })
 // accounts.create({
 //     email: 'dovantuit@gmail.com',
 //     password:'123456',
-//     full_name: 'do van tu',
-//     roll: 'cong tac vien',
-//     phone_number: '0947213743',
-//     address: '42 nguyen van bao, go vap',
-//     is_active: true
-
+//     full_name: 'do van tu'
+//     birthday: '2019-10-09 19:00:00-05'
 // }).then(accounts => console.log(accounts.get({ plain: true })))
 //     .catch(err => console.log(err.message))
 
-const students = db.define('students', { // model : table in database
-    email: sequelize.STRING,
-    full_name: sequelize.STRING,
-    phone_number: sequelize.STRING,
-    address: sequelize.STRING,
-    attended: sequelize.BOOLEAN,
-    createBy: sequelize.STRING,
-    updateBy: sequelize.STRING,
-    is_delete: sequelize.BOOLEAN,
-    is_sentMail: sequelize.BOOLEAN
-})
-// students.create({
-//     email: 'dovantuit@gmail.com',
-//     full_name: 'do van tien',
-//     phone_number: '094723413',
-//     address: '42 nguyen van troi',
-//     attended: false,
-//     createBy: 'do van tu',
-//     updateBy: 'do van tu',
-//     is_delete: false,
-//     is_sentMail: false
-// }).then(students => console.log(students.get({ plain: true })))
-//     .catch(err => console.log(err.message))
-
-
-
 // db.sync() // tao model
-//     .then(() => console.log('>>>>>> tao thanh cong'))
+//     .then(() => console.log('>>>>>>all models tao thanh cong'))
 
 
 
-module.exports = { accounts, students }
+module.exports = {accounts}
--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
-const { accounts, students } = require("./db.js");
-const { transporter } = require("./main"); // để gửi mail
+const { accounts, students } = require("./db.js"); // connect database
+const { transporter } = require("./main"); //  email setting
 
 var server = require("http").Server(app);
-server.listen(process.env.PORT || 3000, () => console.log(">>> server chay port 3000"));
-app.use(require("body-parser").json());
+server.listen(process.env.PORT || 3000, () => console.log(">>> server run port 3000"));
+app.use(require("body-parser").json()); // allow input json type
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.get("/",function(req,res){
-//     res.render("trangchu");
-// });
-// app.get("/", )
 
-app.get("/", (req, res) => res.send("xin chao"));
+app.get("/", (req, res) => res.send("xin chao ban dang truy cap server"));
 ("use strict");
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer"); // pakage support mailing
 const path = require("path");
 const ABSPATH = path.dirname(process.mainModule.filename); // Absolute path to our app directory
 
@@ -26,60 +22,22 @@ app.get("/accounts_read", (req, res) => {
     .then(accounts => res.json({ kq: 1, accounts: accounts }))
     .catch(err => res.json({ kq: 0 }));
 });
+
 // add accounts
 app.post("/accounts_add", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   let full_name = req.body.full_name;
-  let roll = req.body.roll;
-  let phone_number = req.body.phone_number;
-  let address = req.body.address;
-  // let is_active = req.body.is_active
+  let birthday = req.body.birthday;
   accounts
     .create({
       email: email,
       password: password,
       full_name: full_name,
-      roll: roll,
-      phone_number: phone_number,
-      address: address,
-      is_active: true
+      birthday: birthday
     })
     .then(() => res.json({ kq: 1 }))
     .catch(err => res.json({ kq: 0 }));
-
-  // gửi mail
-  transporter.verify(function (error, success) {
-    // Nếu có lỗi.
-    if (error) {
-      console.log(error);
-    } else {
-      //Nếu thành công.
-      // console.log(`>>>>> Kết nối thành công tới ${email}`);
-      transporter.sendMail(
-        {
-          from: "homelesshacker2060@gmail.com",
-          to: email,
-          subject: `TẠO TÀI KHOẢN THÀNH CÔNG`,
-          html:
-            `<p>Xin chào <b>${full_name}</b>. Chúc mừng bạn đã tạo tài khoản thành công, thông tin tài khoản của bạn là:</p><ul><li>Username: ` +
-            email +
-            `</li><li>Passwork: ` +
-            password +
-            `</li></ul>`
-        },
-        (error, info) => {
-          if (error) {
-            console.log(">>> error");
-          } else {
-            console.log(`>>> gửi mail thành công cho ${email}`);
-          }
-        }
-      );
-    }
-  });
-
-  //
 });
 // update accounts
 app.post("/accounts_update", (req, res) => {
@@ -112,7 +70,7 @@ app.post("/accounts_update", (req, res) => {
     .then(row => res.json({ kq: 1, row: row[0] }))
     .catch(err => res.json({ kq: 0 }));
 });
-// delette accounts
+// delete accounts
 app.post("/accounts_update", (req, res) => {
   const {
     id,
@@ -135,255 +93,6 @@ app.post("/accounts_update", (req, res) => {
         phone_number: phone_number,
         address: address,
         is_active: false
-      },
-      {
-        where: { id: id }
-      }
-    )
-    .then(row => res.json({ kq: 1, row: row[0] }))
-    .catch(err => res.json({ kq: 0 }));
-});
-// get list students
-app.get("/students_read", (req, res) => {
-  students
-    .findAll()
-    .then(students => res.json({ kq: 1, students: students }))
-    .catch(err => res.json({ kq: 0 }));
-});
-// add students
-app.post("/students_add", (req, res) => {
-  let email = req.body.email;
-  let full_name = req.body.full_name;
-  let phone_number = req.body.phone_number;
-  let address = req.body.address;
-  let attended = req.body.is_active;
-  let createBy = req.body.createBy;
-  let updateBy = req.body.updateBy;
-  let is_delete = req.body.is_delete;
-
-  students
-    .create({
-      email: email,
-      full_name: full_name,
-      phone_number: phone_number,
-      address: address,
-      attended: false,
-      createBy: createBy,
-      updateBy: updateBy,
-      is_delete: false,
-    })
-    .then(() => res.json({ kq: 1 }))
-    .catch(err => res.json({ kq: 0 }));
-
-  // gửi mail
-  transporter.verify(function (error, success) {
-    // Nếu có lỗi.
-    if (error) {
-      console.log(error);
-    } else {
-      //Nếu thành công.
-      // console.log(`>>>>> Kết nối thành công tới ${email}`);
-      var dataforQR_code = {
-        email: email,
-        phone_number: phone_number,
-        full_name: full_name
-      };
-      transporter.sendMail(
-        {
-          from: "homelesshacker2060@gmail.com",
-          to: email,
-          subject: `UNIT GỬI VÉ MỜI QR CODE CHO BẠN`,
-          // text: `Xin chào ${full_name.toUpperCase()}   Đây là QR code dùng để check in của bạn, khi đi vui lòng mang theo để check in, xin cảm ơn ${code}`,
-          // html: `Xin chào <h1>${full_name.toUpperCase()}</h1><img src="https://i.imgur.com/HTxyz4T.png" alt="Smiley face" height="42" width="42"><p>Đây là QR code dùng để check in của bạn, khi đi vui lòng mang theo để check in, xin cảm ơn</b><ul><li>Username:` + `tom` + `</li><li>Email:` + email + `</li><li>Username:` + phone_number + `</li></ul>`,
-          html: `
-                <html>
-                <head>
-                    <title>Testing QR code</title>
-                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-                    <script type="text/javascript">
-                        function generateBarCode()
-                        {
-                            var nric = $('#text').val();
-                            var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + nric + '&amp;size=50x50';
-                            $('#barcode').attr('src', url);
-                        }
-                    </script>
-                </head>
-                <body>
-                    <p>Xin chào <b>${full_name}</b>. Chúc mừng bạn đã đăng kí tham dự hội thảo ABC thành công, đây là QR code dùng để check in của bạn, khi đi vui lòng mang theo để check in, xin cảm ơn.</p><br><br>
-                    <div style="border: 2px solid black;">
-                    <h4 style=" text-align:center">VÉ MỜI</h4>
-                    <img style=" margin-left: 85px;"
-                        id='barcode' 
-                        src=https://api.qrserver.com/v1/create-qr-code/?data=${JSON.stringify(dataforQR_code)}&amp;size=300x300" 
-                        alt="QR code" 
-                        title="QR code" 
-                        width="150" 
-                        height="150" 
-                    />
-                        <h4 style="text-align:center">SCAN NOW</h4>
-                        </div>
-
-                </body>
-                </html>`
-          // attachments: [
-          //   {
-          //     path: ABSPATH + '/test-data.csv'
-          //   }
-          // ]
-        },
-        (error, info) => {
-          if (error) {
-            console.log(">>> error");
-          } else {
-            console.log(`>>> gửi mail thành công cho ${email}`);
-          }
-        }
-      );
-    }
-  });
-
-  //
-});
-// gửi mail hàng loạt
-app.post("/students_mail", (req, res) => {
-  const { id, email, full_name, phone_number, updateBy } = req.body;
-  students
-    .update(
-      {
-        email: email,
-        full_name: full_name,
-        phone_number: phone_number,
-        updateBy: updateBy
-      },
-      {
-        where: { id: id }
-      }
-    )
-    .then(row => res.json({ kq: 1, row: row[0] }))
-    .catch(err => res.json({ kq: 0 }));
-
-  // gửi mail
-  transporter.verify(function (error, success) {
-    // Nếu có lỗi.
-    if (error) {
-      console.log(error);
-    } else {
-      //Nếu thành công.
-      // console.log(`>>>>> Kết nối thành công tới ${email}`);
-      var dataforQR_code = {
-        email: email,
-        phone_number: phone_number,
-        full_name: full_name
-      };
-      transporter.sendMail(
-        {
-          from: "homelesshacker2060@gmail.com",
-          to: email,
-          subject: `UNIT GỬI VÉ MỜI QR CODE CHO BẠN`,
-          html: `
-                <html>
-                <head>
-                    <title>Testing QR code</title>
-                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-                    <script type="text/javascript">
-                        function generateBarCode()
-                        {
-                            var nric = $('#text').val();
-                            var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + nric + '&amp;size=50x50';
-                            $('#barcode').attr('src', url);
-                        }
-                    </script>
-                </head>
-                <body>
-                    <p>Xin chào <b>${full_name}</b>. Chúc mừng bạn đã đăng kí tham dự hội thảo ABC thành công, đây là QR code dùng để check in của bạn, khi đi vui lòng mang theo để check in, xin cảm ơn.</p><br><br>
-                    <div style="border: 2px solid black;">
-                    <h4 style=" text-align:center">VÉ MỜI</h4>
-                    <img style=" margin-left: 85px;"
-                        id='barcode' 
-                        src=https://api.qrserver.com/v1/create-qr-code/?data=${JSON.stringify(dataforQR_code)}&amp;size=300x300" 
-                        alt="QR code" 
-                        title="QR code" 
-                        width="150" 
-                        height="150" 
-                    />
-                        <h4 style="text-align:center">SCAN NOW</h4>
-                        </div>
-
-                </body>
-                </html>`
-        },
-        (error, info) => {
-          if (error) {
-            console.log(">>> error sent mail");
-          } else {
-            console.log(`>>> đã gửi mail cho học sinh ${email}`);
-          }
-        }
-      );
-    }
-  });
-
-  //
-});
-// update students
-app.post("/students_update", (req, res) => {
-  const {
-    id,
-    email,
-    full_name,
-    phone_number,
-    address,
-    attended,
-    createBy,
-    updateBy,
-    is_delete
-  } = req.body;
-  students
-    .update(
-      {
-        id: id,
-        email: email,
-        full_name: full_name,
-        phone_number: phone_number,
-        address: address,
-        attended: attended,
-        createBy: createBy,
-        updateBy: updateBy,
-        is_delete: is_delete
-      },
-      {
-        where: { id: id }
-      }
-    )
-    .then(row => res.json({ kq: 1, row: row[0] }))
-    .catch(err => res.json({ kq: 0 }));
-});
-// delete students
-app.post("/students_delete", (req, res) => {
-  const {
-    id,
-    email,
-    full_name,
-    phone_number,
-    address,
-    attended,
-    createBy,
-    updateBy,
-    is_delete
-  } = req.body;
-  students
-    .update(
-      {
-        id: id,
-        email: email,
-        full_name: full_name,
-        phone_number: phone_number,
-        address: address,
-        attended: attended,
-        createBy: createBy,
-        updateBy: updateBy,
-        is_delete: true
       },
       {
         where: { id: id }
@@ -485,78 +194,4 @@ app.get("/gg_read", (req, res) => {
       .then(rows => res.json({ kq: 1, rows: rows.data }))
       .catch(err => res.json({ kq: 0 }));
   }
-});
-// gửi mail báo cáo
-
-
-
-app.post("/report_mail", (req, res) => {
-
-
-  const { email, data } = req.body;
-  // tao file excel
-  const csvjson = require('csvjson');
-  const readFile = require('fs').readFile;
-  const writeFile = require('fs').writeFile;
-  //// xử lí load dữ liệu từ postgresSQL
-  // var data_from_postgres = []
-  students
-    .findAll()
-    .then(students => res.json({ kq: 1, data_from_postgres: students }))
-    // .then(students => console.log(students))
-    .catch(err => res.json({ kq: 0 }));
-  // console.log(JSON.stringify(data_from_postgres))
-
-
-
-
-  //
-
-  // readFile(`./test_data.json`, 'utf-8', (err, fileContent) => {
-  //   if (err) {
-  //     console.log(err); // Do something to handle the error or just throw it
-  //     throw new Error(err);
-  //   }
-  const csvData = csvjson.toCSV(data, {
-    headers: 'key'
-  });
-  writeFile(`./${email}.csv`, csvData, (err) => {
-    if (err) {
-      console.log(err); // Do something to handle the error or just throw it
-      throw new Error(err);
-    }
-    console.log('Success!');
-  });
-  // });
-
-  // gửi mail
-  transporter.verify(function (error, success) {
-    // Nếu có lỗi.
-    if (error) {
-      console.log(error);
-    } else {
-      transporter.sendMail(
-        {
-          from: "homelesshacker2060@gmail.com",
-          to: email,
-          subject: `UNIT GỬI BÁO CÁO EXCEL CHO BẠN`,
-          text: 'Đây là file báo cáo EXCEL',
-          attachments: [
-            {
-              path: ABSPATH + `/${email}.csv`
-            }
-          ]
-        },
-        (error, info) => {
-          if (error) {
-            console.log(">>> error sent mail");
-            res.json({ kq: 0 })
-          } else {
-            console.log(`>>> đã gửi BÁO CÁO đến ${email}`);
-            res.json({ kq: 1 })
-          }
-        }
-      )
-    }
-  });
 });
